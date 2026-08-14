94d91055666069baa95246fe526531ea14637f28
Shreyas Vavley <https://github.com/ShreyasVavley>
2026-08-14 11:47:47 +0530

    style: speed up lenis smooth scrolling physics

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -5,7 +5,7 @@ import { ReactLenis } from 'lenis/react';
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.08, duration: 1.5, smoothWheel: true }}>
+    <ReactLenis root options={{ lerp: 0.15, smoothWheel: true, wheelMultiplier: 1.2 }}>
       {children}
     </ReactLenis>
   );
